f998633c0683890ec546f5da3ac13712ea7e5fbf
刘京坤 <2802629917@qq.com>
2025-10-01 17:05:54 +0800

    完成项目基础架构构建以及工具的引入

---
+++ vue-deepseek/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+::-webkit-scrollbar {
+    /* 设置滚动条宽度 */
+    width: 7px;
+    /* 隐藏滚动条 */
+    display: none;
+}
+
+</style>

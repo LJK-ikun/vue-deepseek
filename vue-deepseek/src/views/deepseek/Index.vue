59449fef124568aada779ea6edd3fbc5c6cd821d
刘京坤 <2802629917@qq.com>
2025-10-05 17:15:22 +0800

    写了不少bug没修复

--- vue-deepseek/src/views/deepseek/Index.vue
+++ vue-deepseek/src/views/deepseek/Index.vue
@@ -44,7 +44,7 @@
                             handleRequest();
                         }" show-word-limit/>
 
-                        <el-button style="height: 40px;" type="primary" class="send-btn" :loading="loading" :disable="!queryKeys" @click="handleRequest">
+                        <el-button style="height: 40px;" type="primary" class="send-btn" :loading="loading" :disabled="!queryKeys" @click="handleRequest">
                             <el-icon><Promotion/></el-icon>
                         </el-button>
                     </div>
@@ -122,7 +122,7 @@ const initIndex = () => {
 }
 
 // 新建会话
-const handleAddSession = () => {
+const handleChangeSessionIndex = () => {
     // 检查是否有查询正在进行
     if(loading.value){
         ElMessage.warning('请先完成当前查询')
@@ -135,11 +135,32 @@ const handleAddSession = () => {
         messages: []
     })
     //清空查询信息列表，准备信息对话
-    queryInfos.value.messages = []
+    queryInfos.value.message = []
     //设置活动对话索引为新添加的对话索引
     activeIndex.value = sessionList.value.length - 1
 }
 
+//编辑对话框
+const handleFocusIndex = (index: number) => {
+    //设置编辑索引为当前传入的索引
+    editIndex.value = index;
+}
+
+//切换对话框
+const handleChangeSessionIndex = async(index: number) => {
+    if (loading.value) {
+        ElMessage.warning('请先完成当前查询')
+        return;
+    }
+    activeIndex.value = index;
+    queryInfos.value.message = sessionList.value[activeIndex.value].messages || [];
+    // 等待消息组件渲染完成
+    await nextTick();
+    // 滚动到消息底部
+    if (messageRef.value){
+        messageRef.value.scrollToBottom();
+    }
+}
 // 组件挂载完成
 onMounted(() => {
     initSessionList()
@@ -205,6 +226,83 @@ onMounted(() => {
 .grid-space-between{
     width: 100%;
     height: calc(100% - 40px);
+}
 
+/* 左边样式 start */
+.left-container{
+    background-color: #2f2f2f;
+    padding: 16px;
+    border-radius: 8px;
+    height: calc(94vh - 40px - 32px);
+}
+
+.left-container .session-area{
+    margin-top: 16px;
+    height:calc(100% - 56px);
+    overflow-y: auto;
+}
+
+.left-container .session-item{
+    display: flex;
+    justify-content: space-between;
+    padding: 8px 12px;
+    align-items: center;
+    margin-bottom: 8px;
+    border-radius: 6px;
+    background: rgba(31,31,31,0.6);
+    color: #fff;
+    /* 设置光标为指针 */
+    cursor: pointer;
+    transition: all 0.3s ease;
+}
+
+.left-container .session-item-active{
+    /* 设置背景颜色色为深灰色，透明度为0.2 */
+    background: rgba(198,216,130,0.2);
+    border: 1px solid rgba(254,44,85,0.3);
+}
+
+.session-item .icon-box{
+    display: flex;
+    gap: 8px;
+    opacity: 0;
+    transition: opacity 0.3s ease;
+}
+
+.session-item .icon-box:hover{
+    opacity: 1;
+}
+
+.icon-box .icon{
+    font-size: 16px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+}
+
+.icon:hover{
+    /* 设置缩放为1.2 */
+    transform: scale(1.2);
+    color: #e99d53!important;
+}
+/* 按钮样式 */
+:deep(.el-button){
+    border-radius: 8px;
+    transition: all 0.3s ease;
+}
+
+:deep(.el-button:hover){
+    transform: translateY(-1px);
+
+    box-shadow: 0 0 12px rgba(208,204,8,0.5);
+}
+
+:deep(.el-button:active){
+    /* 按钮点击时y轴偏移1px */
+    transform: translateY(1px);
+}
+
+:deep(.el-button.is-disabled){
+    background-color: rgba(208, 204, 8, 0.5);
+    border-color: rgba(208, 204, 8, 0.5);
 }
 </style>
